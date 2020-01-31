3628043102ef00af2f592b905ea002e6e377e23f
marcin <marcin@zaczynski.pl>
2020-02-01 00:01:07 +0100

    zainstalowane npgsql

---
+++ ecoplastol/ecoplastol.WebUI/Models/LoginPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ecoplastol.WebUI.Models
+{
+    public class LoginPageViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+
+        [Required]
+        [Display(Name = "Maszyna")]
+        public string Maszyna { get; set; }
+
+        [Required]
+        [Display(Name = "Zmiana")]
+        public string Zmiana { get; set; }
+
+        [Required]
+        [Display(Name = "Hasło")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
